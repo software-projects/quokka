1e60caff6e361d88642885fddc200e2f1c094bc7
John Jeffery <jjeffery@sp.com.au>
2009-08-15 06:56:07 +0000

    Add comment about how this interface is probably going to change.

--- src/Quokka.Core/ServiceLocation/IServiceContainer.cs
+++ src/Quokka.Core/ServiceLocation/IServiceContainer.cs
@@ -61,15 +61,13 @@ namespace Quokka.ServiceLocation
 		/// <returns>The <see cref="IServiceContainer"/>.</returns>
 		IServiceContainer RegisterType(Type from, Type to, string name, ServiceLifecycle lifecycle);
 
-
-
-
 		/// <summary>
 		/// Register an instance of an object with the container.
 		/// </summary>
 		/// <param name="from">
 		/// The type that is requested from the container. Often this is an interface type, but it can be
-		/// a concrete type that is the type of <paramref name="instance"/>, or a supertype of <paramref name="instance"/>.
+		/// a concrete type that is the type of <paramref name="instance"/>, or a supertype of the type of
+		/// <paramref name="instance"/>.
 		/// </param>
 		/// <param name="name">
 		/// The name associated with the mapping, or <c>null</c> if this is the default mapping.
@@ -89,6 +87,12 @@ namespace Quokka.ServiceLocation
 		/// <param name="type">Type registered with the container</param>
 		/// <param name="name">Name registered, or <c>null</c> if the default mapping.</param>
 		/// <returns></returns>
+		/// <remarks>
+		/// This may change in a future version. Need to investigate the behaviour of different
+		/// containers more. Probably change this to just IsTypeRegistered(Type type), as that
+		/// is all the framework needs (if that), and that is something that all containers can
+		/// probably do as a minimum. 
+		/// </remarks>
 		bool IsTypeRegistered(Type type, string name);
 
 	}
