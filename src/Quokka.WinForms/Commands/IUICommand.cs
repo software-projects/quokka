5e8e40cfd15967d3ca8eada44f6d175b50227de3
John Jeffery <jjeffery@sp.com.au>
2009-08-20 23:34:45 +0000

    Add commands classes and interface.

---
+++ src/Quokka.WinForms/Commands/IUICommand.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Quokka.WinForms.Commands
+{
+	/// <summary>
+	/// Represents a command that can be invoked from a UI control.
+	/// </summary>
+	/// <remarks>
+	/// Acknowledgement to Component Factory Krypton Toolkit for the 
+	/// basic idea. Implementation differs slightly.
+	/// </remarks>
+	public interface IUICommand : INotifyPropertyChanged
+	{
+		bool Checked { get; set; }
+		CheckState CheckState { get; set; }
+		bool Enabled { get; set; }
+		string Text { get; set; }
+		Image ImageLarge { get; set; }
+		Image ImageSmall { get; set; }
+		Color ImageTransparentColor { get; set; }
+
+		void Execute();
+	}
+}
