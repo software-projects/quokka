ceef2fdd040a265580f333bc8d71b3548b31b101
John Jeffery <jjeffery@sp.com.au>
2011-10-31 23:34:16 +0000

    Restore Sprocket.Service project.

---
+++ src/Sprocket.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using Sprocket.Service.ServiceProcess;
+
+namespace Sprocket.Service
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WindowsService.Run<SprocketService>(args);
+		}
+	}
+}
