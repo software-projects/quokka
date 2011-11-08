77657a86e1d1c72717e22f6654ea34f2e22eee7e
John Jeffery <jjeffery@sp.com.au>
2011-11-08 01:09:59 +0000

    Work on database migrations.

---
+++ src/Quokka.Core/Data/Migrations/DatabaseMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace Quokka.Data.Migrations
+{
+	public class DatabaseMigration
+	{
+		public string Module { get; set; }
+		public string Version { get; set; }
+		public string Description { get; set; }
+		public DateTime CreatedAt { get; set; }
+
+		public override int GetHashCode()
+		{
+			return Version.GetHashCode()
+				   ^ Module.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			var other = obj as DatabaseMigration;
+			if (other == null)
+			{
+				return false;
+			}
+
+			return StringComparer.InvariantCultureIgnoreCase.Compare(Version, other.Version) == 0
+				&& StringComparer.InvariantCultureIgnoreCase.Compare(Module, other.Module) == 0;
+		}
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			sb.Append(Module);
+			sb.Append("/");
+			sb.Append(Version);
+			sb.Append("_");
+			sb.Append(Description);
+			sb.Append(".sql");
+			return sb.ToString();
+		}
+	}
+}
---
+++ src/Quokka.Core/Data/Migrations/DbCommandExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Common.Logging;
+
+namespace Quokka.Data.Migrations
+{
+	internal static class DbCommandExtensions
+	{
+		private static readonly ILog Logger = LogManager.GetCurrentClassLogger();
+
+		private static readonly Regex RowcountRegex = new Regex(@"^\s*--\s*repeat\s+until\s+rowcount\s+=+\s+0",
+														RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+		private static readonly Regex RowcountRegex2 = new Regex(@"^\s*set\s+rowcount\s+\d+", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+		public static void RunMigrationScript(this IDbCommand cmd, TextReader reader)
+		{
+			var sb = new StringBuilder();
+
+			for (; ; )
+			{
+				string line = reader.ReadLine();
+				if (line == null)
+				{
+					RunMigrationSql(cmd, sb.ToString());
+					break;
+				}
+
+				if (line.Trim().ToLower() == "go")
+				{
+					RunMigrationSql(cmd, sb.ToString());
+					sb = new StringBuilder();
+				}
+				else
+				{
+					sb.AppendLine(line);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Run an SQL script for a database migration. If the script starts with 'set rowcount nn', then the sql will be run
+		/// repeatedly until there are no rows updated.
+		/// </summary>
+		/// <param name="cmd"></param>
+		/// <param name="sql"></param>
+		public static void RunMigrationSql(this IDbCommand cmd, string sql)
+		{
+			if (String.IsNullOrEmpty(sql))
+			{
+				return;
+			}
+
+			if (sql.Length == 0)
+			{
+				return;
+			}
+
+			cmd.CommandType = CommandType.Text;
+			cmd.CommandText = sql;
+
+			if (RowcountRegex.IsMatch(sql) || RowcountRegex2.IsMatch(sql))
+			{
+				int totalRowCount = 0;
+				for (; ; )
+				{
+					int rowCount = cmd.ExecuteNonQuery();
+					if (rowCount == 0)
+					{
+						break;
+					}
+					totalRowCount += rowCount;
+					Logger.InfoFormat("... {0} rows updated", totalRowCount);
+				}
+
+				// restore any rowcount settting
+				cmd.CommandText = "set rowcount 0";
+				cmd.ExecuteNonQuery();
+			}
+			else
+			{
+				cmd.ExecuteNonQuery();
+			}
+		}
+	}
+}
