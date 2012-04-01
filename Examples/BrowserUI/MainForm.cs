c2297f325cb8d807318de37be2db78710c72af6e
John Jeffery <jjeffery@sp.com.au>
2012-04-01 07:01:16 +0000

    Work on custom pluggable protocol for embedded IE web browsers.

---
+++ Examples/BrowserUI/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using Quokka.UI.WebBrowsers;
+
+namespace BrowserUI
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainFormLoad(object sender, EventArgs e)
+		{
+			PluggableProtocol.Register(Assembly.GetExecutingAssembly());
+			webBrowser.Navigate("quokka://host/index.html");
+		}
+	}
+}
---
+++ src/Quokka.Core/UI/WebBrowsers/EmbeddedResourceMap.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Quokka.UI.WebBrowsers
+{
+	public class EmbeddedResourceMap
+	{
+		private readonly HashSet<Assembly> _assemblies = new HashSet<Assembly>();
+		private Lazy<Dictionary<string, EmbeddedResource>> _lazy;
+
+
+		public EmbeddedResourceMap()
+		{
+			ClearDict();
+		}
+
+		public void AddAssembly(Assembly assembly)
+		{
+			if (!_assemblies.Contains(assembly))
+			{
+				_assemblies.Add(assembly);
+				ClearDict();
+			}
+		}
+
+		public Stream GetStream(string name)
+		{
+			Uri uri = new Uri(name);
+
+			var path = uri.AbsolutePath;
+			var pieces = path.Split('/');
+			var fileName = pieces[pieces.Length - 1];
+
+			EmbeddedResource manifestInfo = null;
+			if (_lazy.Value.TryGetValue(fileName, out manifestInfo))
+			{
+				return manifestInfo.Assembly.GetManifestResourceStream(manifestInfo.ResourceName);
+			}
+			return null;
+		}
+
+		private void ClearDict()
+		{
+			_lazy = new Lazy<Dictionary<string, EmbeddedResource>>(CreateDict, true);
+		}
+
+		private static readonly string[] Suffixes = new[]
+		                                            	{
+		                                            		".html",
+		                                            		".htm",
+		                                            		".js",
+		                                            		".css",
+		                                            		".png",
+		                                            		".jpeg",
+		                                            		".gif",
+		                                            	};
+
+		private static bool IsValidResource(string name)
+		{
+			return Suffixes.Any(name.EndsWith);
+		}
+
+		private Dictionary<string, EmbeddedResource> CreateDict()
+		{
+			var dict = new Dictionary<string, EmbeddedResource>(StringComparer.InvariantCultureIgnoreCase);
+
+			foreach (var assembly in _assemblies)
+			{
+				foreach (var resourceName in assembly.GetManifestResourceNames())
+				{
+					if (IsValidResource(resourceName))
+					{
+						var embeddedResource = new EmbeddedResource(assembly, resourceName);
+
+						// split name into bits separated by periods
+						var nameParts = resourceName.Split('.');
+
+						var fileName = nameParts[nameParts.Length - 2] + "." + nameParts[nameParts.Length - 1];
+
+						dict[fileName] = embeddedResource;
+
+						// TODO: add alternatives that include the rest of the path
+					}
+				}
+			}
+			return dict;
+		}
+
+		private class EmbeddedResource
+		{
+			public Assembly Assembly { get; private set; }
+			public string ResourceName { get; private set; }
+
+			public EmbeddedResource(Assembly assembly, string resourceName)
+			{
+				Assembly = assembly;
+				ResourceName = resourceName;
+			}
+		}
+	}
+}
