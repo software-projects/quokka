5f93914b49516888ee6b15b8653ccd05930d5b15
John Jeffery <jjeffery@sp.com.au>
2012-08-01 02:28:13 +0000

    Add DisposableExtensions class, with method DisposeWith.

---
+++ src/Quokka.Core/Util/DisposableExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using Quokka.Diagnostics;
+
+namespace Quokka.Util
+{
+	public static class DisposableExtensions
+	{
+		/// <summary>
+		/// Causes this object to be disposed when the <see cref="Component"/> is disposed.
+		/// </summary>
+		/// <param name="disposable">
+		///		The <see cref="IDisposable"/> object that</param> should be disposed when
+		///		the <paramref name="component"/> is disposed.
+		/// <param name="component">
+		///		When this component is disposed, the <paramref name="disposable"/> object
+		///		should be disposed.
+		/// </param>
+		/// <remarks>
+		///		This is useful for ensuring that objects are disposed when a Windows Forms
+		///		control is disposed.
+		/// </remarks>
+		public static void DisposeWith(this IDisposable disposable, Component component)
+		{
+			if (disposable == null)
+			{
+				return;
+			}
+
+			component.Disposed += (sender, args) => DisposeOf(disposable);
+		}
+
+		private static void DisposeOf(IDisposable disposable)
+		{
+			try
+			{
+				disposable.Dispose();
+			}
+			catch (ObjectDisposedException ex)
+			{
+				// not supposed to happen, but sometimes does
+				var logger = LoggerFactory.GetCurrentClassLogger();
+				var msg = string.Format("Object of type {0} raised ObjectDisposedException during disposal",
+				                        disposable.GetType());
+				logger.Warn(msg, ex);
+			}
+			catch (Exception ex)
+			{
+				var logger = LoggerFactory.GetCurrentClassLogger();
+				var msg = string.Format("Object of type {0} raised an exception during disposal: {1}",
+				                        disposable.GetType(),
+				                        ex.Message);
+				logger.Error(msg, ex);
+			}
+		}
+	}
+}
