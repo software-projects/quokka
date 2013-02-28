9d3d54da339e3a56f155aff46908eacfe993616a
John Jeffery <jjeffery@sp.com.au>
2013-02-28 02:35:56 +0000

    Add ICurrentUser, Permission, DefaultRole.

--- src/Quokka.Core/Security/ICurrentUser.cs
+++ src/Quokka.Core/Security/ICurrentUser.cs
@@ -14,8 +14,17 @@ namespace Quokka.Security
 		string Description { get; }
 
 		/// <summary>
+		/// This is normally the name that the user enters during login.
+		/// </summary>
+		string Username { get; }
+
+		/// <summary>
 		/// Unique identification of the user. This can be any type. Common types include integer, string and Guid.
 		/// </summary>
+		/// <remarks>
+		/// This might be the same as <see cref="Username"/>, or it might be the underlying identifier/guid that is used to 
+		/// uniquely identify the user.
+		/// </remarks>
 		object UserId { get; }
 
 		/// <summary>
@@ -27,5 +36,18 @@ namespace Quokka.Security
 		/// Does the user have any of the specified permissions.
 		/// </summary>
 		bool HasAnyPermission(params Permission[] permissions);
+	}
+
+	public static class CurrentUserExtensions
+	{
+		/// <summary>
+		/// Is there a current user logged in.
+		/// </summary>
+		/// <param name="currentUser"></param>
+		/// <returns></returns>
+		public static bool IsLoggedIn(this ICurrentUser currentUser)
+		{
+			return (currentUser != null && currentUser.UserId != null);
+		}
 	}
 }
