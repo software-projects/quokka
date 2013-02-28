a60ba69eb172b5bd04c1fedbd11e3985cdbe31e8
John Jeffery <jjeffery@sp.com.au>
2013-02-28 02:22:33 +0000

    Add ICurrentUser, Permission, DefaultRole.

---
+++ src/Quokka.Core/Security/DefaultRole.cs
@@ -0,0 +1,42 @@
+﻿using Quokka.Diagnostics;
+
+namespace Quokka.Security
+{
+	/// <summary>
+	/// A role is usually associated with a job description, and is used to group together permissions (see <see cref="Permission"/>).
+	/// </summary>
+	/// <remarks>
+	/// Roles are application-specific. An application will typically have a few pre-defined roles, and it can be useful to
+	/// assign <see cref="Permission"/> objects to one or more <see cref="Permission"/> objects so that a default set of <see cref="DefaultRole"/> - <see cref="DefaultRole"/>
+	/// mappings can be pre-configured in a security access database during installation of the product.
+	/// </remarks>
+	/// <example>
+	/// <code>
+	///		// Default roles referenced by permissions.
+	///		public static class DefaultRoles
+	///		{
+	///			public static readonly Role Client = new Role("CLI", "Clients of the company");
+	///			public static readonly Role Staff = new Role("STF", "All current staff of the company");
+	///			public static readonly Role Supervisor = new Role("SUP", "Supervisors");
+	///		}
+	/// </code>
+	/// </example>
+	public class DefaultRole
+	{
+		/// <summary>
+		/// Uniquely identifies the role. Used for persistent storage (eg database primary key).
+		/// </summary>
+		public string Id { get; private set; }
+
+		/// <summary>
+		/// Describes the role briefly. Use a job description or similar (eg "Shift Supervisor")
+		/// </summary>
+		public string Description { get; private set; }
+
+		public DefaultRole(string id, string description)
+		{
+			Id = Verify.ArgumentNotNull(id, "id");
+			Description = Verify.ArgumentNotNull(description, "description");
+		}
+	}
+}
---
+++ src/Quokka.Core/Security/ICurrentUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Quokka.Security
+{
+	/// <summary>
+	/// Interface used to 
+	/// </summary>
+	public interface ICurrentUser
+	{
+		/// <summary>
+		/// This is either the name of the individual (eg "John CITIZEN"), or else if the user does not correspond
+		/// to an individual, it is the description of the role (eg "Administration User").
+		/// </summary>
+		string Description { get; }
+
+		/// <summary>
+		/// Unique identification of the user. This can be any type. Common types include integer, string and Guid.
+		/// </summary>
+		object UserId { get; }
+
+		/// <summary>
+		/// Does this user have the specified permission.
+		/// </summary>
+		bool HasPermission(Permission permission);
+
+		/// <summary>
+		/// Does the user have any of the specified permissions.
+		/// </summary>
+		bool HasAnyPermission(params Permission[] permissions);
+	}
+}
